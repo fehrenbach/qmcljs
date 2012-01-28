ac5ab157d8507cd45a5dc8872b59f00c3e505372
Stefan Fehrenbach <stefan.fehrenbach@gmail.com>
2012-01-28 17:11:07 +0100

    Give jaki an absolute URL
    We are not a CouchApp, that is a web application served by CouchDB directly,
    so we cannot rely on relative URLs.

--- examples/library-use-jaki/src/foo/couches.cljs
+++ examples/library-use-jaki/src/foo/couches.cljs
@@ -1,6 +1,9 @@
 (ns foo
   (:require [jaki.couch :as couch]))
 
+;; use absolute URL because we are not served by a couchdb
+(reset! jaki.couch/*url-prefix* "http://127.0.0.1:5984")
+
 ;; stolen from pinot
 ;; https://github.com/ibdknox/pinot/blob/master/src/pinot/util/js.cljs
 (defn map->js [m]
